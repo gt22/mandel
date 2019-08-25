b2e002726892faa4ff6219a7b03127f2efef0423
gt22 <igorengel@mail.ru>
2019-08-25 15:39:04 +0300

    change version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,24 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.41"
+    id("com.github.johnrengelman.shadow") version "4.0.4"
 }
 
 group = "com.gt22"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     maven("http://npm.mipt.ru:8081/artifactory/gradle-dev")
     maven("https://jitpack.io")
     mavenCentral()
+}
+
+
+
+val jar by tasks.getting(Jar::class) {
+    manifest {
+        attributes["Main-Class"] = "mandel.MainKt"
+    }
 }
 
 dependencies {
